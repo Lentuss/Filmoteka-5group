f8b8aeb232020196daa2400a09ec86cc258eb911
Olena Pohybil <itlentus@gmail.com>
2022-06-16 12:43:38 +0300

    add buttons

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './sass/index.scss';
 
+import './js/fireDb';
+import './js/loginWindow';
+import './js/renderLibrary';
 import './js/getTrendFilmsApiService';
 import './js/renerList';
 import './js/getDetails';
 import './js/getGenres';
-import './js/loginWindow';
-import './js/renderLibrary';
 import './js/apiVariables';
 
 import './js/scrollUp';
