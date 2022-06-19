e037a9c0638cecb8a54dc2cd7fb559a5071705c8
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-06-19 23:52:13 +0300

    Змінює файл з пошуком для уніфікованого АРІ

--- src/js/filmSearch.js
+++ src/js/filmSearch.js
@@ -10,6 +10,7 @@ const formEl = document.querySelector('.header-form');
 const mainBtnsEls = document.querySelector('.main__button-list');
 const loaderEl = document.querySelector('.loader');
 const failedSearch = document.querySelector('.search-error');
+const sliderSection = document.querySelector('.slider-section');
 
 formEl.addEventListener('submit', onFormSubmit);
 
@@ -17,60 +18,56 @@ async function onFormSubmit(e) {
     e.preventDefault();
     
     cleanMarkup();
-
+    
     galleryEl.insertAdjacentHTML('beforeend',`<div class="loader"></div>`);
     loaderEl.style.display = "block";
-    movieAPIService.query = e.currentTarget.elements.searchQuery.value;
+    movieAPIService.query = e.currentTarget.elements.searchQuery.value.trim();
+    movieAPIService.resetPage();
     e.currentTarget.elements.searchQuery.value = "";
+    
     getFilms();
 }
 
 async function getFilms() {
     try {
-
+        
         if (movieAPIService.query !== "") {
-            const movieFromApi = await movieAPIService.getFilms(SEARCH_URL);
-           
-            if (movieFromApi.total_results === 0) {
-
+            const movieFromApi = await movieAPIService.getSearchedFilms(SEARCH_URL);
+            
+            if (movieFromApi.total_results === 0) {                
                 loaderEl.style.display = "none";
                 mainBtnsEls.style.display = "flex";
                 failedSearch.classList.remove("visually-hidden");
-                getTrendFilms();
-
-            } else {
-
-                onGetSucces(movieFromApi);
-
+                getTrendFilms();                
+            } else {                
+                onGetSucces(movieFromApi);                
             }
             
         } else {
-
             mainBtnsEls.style.display = "flex";
             loaderEl.style.display = "none";
             failedSearch.classList.remove("visually-hidden");
-            getTrendFilms();
-
+            getTrendFilms();            
         }
-
+        
     } catch (error) {
         console.log(error.message);
-    }
-    
+    }   
 }
 
 function onGetSucces(movieFromApi) {
-
+    
     const movieForRender = createListMarkup(movieFromApi);
     
+    sliderSection.style.position = 'static';
     failedSearch.classList.add("visually-hidden");
     mainBtnsEls.style.display = "none";
     loaderEl.style.display = "none";
-
+    
     observer.observe(galleryEl.lastElementChild);
 
     galleryEl.insertAdjacentHTML('beforeend', movieForRender);
-
+    
     observer.observe(galleryEl.lastElementChild);
 }
 
@@ -80,19 +77,20 @@ function cleanMarkup() {
 
 // infinite scroll
 
-const options = {
-  intersectionObserver: {
-    root: galleryEl.lastElementChild,
-    rootMargin: "0px 0px 200px 0px",
-    threshold: 1,
-  },
-};
 
+const options = {
+    intersectionObserver: {
+        root: galleryEl.lastElementChild,
+        rootMargin: "0px 0px 200px 0px",
+        threshold: 1,
+        },
+    };
+        
 const callback = function (entries, observer) {
     if (entries[0].isIntersecting) {
-        observer.unobserve(entries[0].target);
-        getFilms(); 
-  }
-};
- 
+            observer.unobserve(entries[0].target);
+            getFilms(); 
+        }
+    };
+                
 const observer = new IntersectionObserver(callback, options.intersectionObserver);
